eaa804e8564c2bcd1154907e9d4a814852e6a4f2
scgr <social.cgr@gmail.com>
2019-10-12 19:52:21 +0200

    End version of the project right after the episode 21 - Modified bootstrap and added thymeleaf template

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,21 +15,12 @@ import guru.springframework.spring5webapp.repositories.PublisherRepository;
 @Component // Anotacion que representa un Bean en Spring
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
+	@Autowired
 	private AuthorRepository authorRepository;
+	@Autowired
 	private BookRepository bookRepository;
+	@Autowired
 	private PublisherRepository publisherRepository;
-
-	/**
-	 * @param authorRepository
-	 * @param bookRepository
-	 */
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,
-			PublisherRepository publisherRepository) {
-		super();
-		this.authorRepository = authorRepository;
-		this.bookRepository = bookRepository;
-		this.publisherRepository = publisherRepository;
-	}
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -39,13 +31,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 	 * Method to initialize the H2 database data
 	 */
 	private void initData() {
-		
+
 		// Publisher
 		Publisher publisher = new Publisher();
 		publisher.setName("Publisher 1");
-		
+
 		publisherRepository.save(publisher);
-		
+
 		// Eric
 		Author eric = new Author("Eric", "Evans");
 		Book b1 = new Book("Domain Driven Design", "01234", publisher);
