f91c46ec469c76aea15b4a165fdf1285b523465d
scgr <social.cgr@gmail.com>
2019-10-12 17:49:05 +0200

    End version of the project right after the episode 17 - Spring Data JPA Repositories

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -9,23 +9,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-@Entity //Anotacion de entidad para relacionarlo con una tabla de base de datos
+@Entity // Anotacion de entidad para relacionarlo con una tabla de base de datos
 public class Author {
 
-	@Id //Anotacion de identificador unico de tabla en la base de datos (PK)
-	@GeneratedValue(strategy = GenerationType.AUTO) //Anotacion de valor generado automaticamente
+	@Id // Anotacion de identificador unico de tabla en la base de datos (PK)
+	@GeneratedValue(strategy = GenerationType.AUTO) // Anotacion de valor generado automaticamente
 	private Long id;
 	private String firstName;
 	private String lastName;
 
-	@ManyToMany(mappedBy = "authors") //Anotacion para indicar que la relacion es muchos a muchos respecto a Book
+	@ManyToMany(mappedBy = "authors") // Anotacion para indicar que la relacion es muchos a muchos respecto a Book
 	private Set<Book> books = new HashSet<>();
 
 	/**
 	 * Default constructor
 	 */
 	public Author() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -80,6 +79,37 @@ public class Author {
 
 	public void setBooks(Set<Book> books) {
 		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
 	}
 
 }
