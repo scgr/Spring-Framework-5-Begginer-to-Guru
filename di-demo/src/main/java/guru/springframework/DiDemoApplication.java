5ce897b84afac1b2756755c8c01cb474463e7db0
scgr <social.cgr@gmail.com>
2019-10-13 16:13:46 +0200

    Initial commit of DI project

---
+++ di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,20 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.controllers.MyController;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
+	}
+
+}
---
+++ di-demo/src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,12 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+	
+	public void hello() {
+		System.out.println("Hello!!!");
+	}
+
+}
