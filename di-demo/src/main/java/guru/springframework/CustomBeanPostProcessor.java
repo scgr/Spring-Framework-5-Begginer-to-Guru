4d072baa07937a2b91bde85e899858523b556bc9
scgr <social.cgr@gmail.com>
2019-10-20 01:29:52 +0200

    Added overriden methods to analise the life cycle of a bean

---
+++ di-demo/src/main/java/guru/springframework/CustomBeanPostProcessor.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+		if (bean instanceof LifeCycleDemoBean) {
+			((LifeCycleDemoBean) bean).afterInit();
+		}
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+		if (bean instanceof LifeCycleDemoBean) {
+			((LifeCycleDemoBean) bean).beforeInit();
+		}
+		return bean;
+	}
+}
