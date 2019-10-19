986170de7dad61b5a8b2aecb6c01c17c39ca22e7
scgr <social.cgr@gmail.com>
2019-10-19 23:38:53 +0200

    Modified controllers and services for adding Profile annotation.

--- di-demo/src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ di-demo/src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary // Anotacion usada para cuando se encuentran varios servicios posibles, este es el primario
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
-		return "Hello - Primary greeting service";
+		return "Hello - Primary greeting service in German (Primärer Begrüßungsdienst)!!!";
 	}
 
 }
