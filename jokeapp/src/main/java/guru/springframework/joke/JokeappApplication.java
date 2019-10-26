e1d65d2b61c160f1227e013a0ca310b61e123f84
scgr <social.cgr@gmail.com>
2019-10-26 14:46:17 +0200

    Refactoring wrong package name

--- jokeapp/src/main/java/guru/springframework/joke/JokeappApplication.java
+++ jokeapp/src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.joke.joke;
+package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
