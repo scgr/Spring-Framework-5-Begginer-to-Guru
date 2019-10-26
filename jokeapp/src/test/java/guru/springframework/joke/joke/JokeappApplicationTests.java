9939119d4af8f70f86f7704fce97f676cd9c2462
scgr <social.cgr@gmail.com>
2019-10-26 14:42:46 +0200

    Starting up new project with Spring Boot

---
+++ jokeapp/src/test/java/guru/springframework/joke/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.joke.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
