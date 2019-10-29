69804e4c8713b4106a4d9e184f81f1106dabb483
scgr <social.cgr@gmail.com>
2019-10-29 19:58:05 +0100

    Added owners html page and controller to the web project

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({ "/vets" })
-public class VetController {
+@RequestMapping({ "/owners" })
+public class OwnerController {
 
-	@RequestMapping({ "", "/", "/listVets", "/listVets.html" })
-	public String listVets() {
-		return "vets/index";
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listOwners() {
+		return "owners/index";
 	}
 
 }
