d687138f561c8bf0493ab324d32c7a3f6d9d4955
scgr <social.cgr@gmail.com>
2019-11-01 13:33:44 +0100

    Modified project to put in practice the declaration of beans using XML configuration files

--- jokeapp-2/src/main/java/guru/springframework/joke/JokeappApplication.java
+++ jokeapp-2/src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
 	public static void main(String[] args) {
--- jokeapp-2/src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ jokeapp-2/src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration // Anotacion para hacer que la clase se encargue de declarar los beans que van a
+//@Configuration // Anotacion para hacer que la clase se encargue de declarar los beans que van a
 				// ser utilizados en el proyecto.
 public class ChuckConfiguration {
 
-	@Bean // Anotacion para indicar que este objeto va a ser un bean usado dentro de la aplicacion.
+	//@Bean // Anotacion para indicar que este objeto va a ser un bean usado dentro de la aplicacion.
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
