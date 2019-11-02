02c8ce767c8c4ea207e8e7d92b03d52aca6bbf6e
scgr <social.cgr@gmail.com>
2019-11-02 03:13:50 +0100

    Listing vets through web using model

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping({ "/vets" })
 public class VetController {
 
-	@RequestMapping({ "", "/", "/listVets", "/listVets.html" })
-	public String listVets() {
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
+
 		return "vets/index";
 	}
 
