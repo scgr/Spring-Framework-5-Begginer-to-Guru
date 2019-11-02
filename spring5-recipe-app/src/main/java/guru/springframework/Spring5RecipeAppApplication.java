eb0ec6ee09c1cc0213c06abef50f34a23e2ffc03
scgr <social.cgr@gmail.com>
2019-11-02 15:45:44 +0100

    Initial commit related to the new project of recipes to put into practice the Spring MVC

---
+++ spring5-recipe-app/src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+	}
+
+}
