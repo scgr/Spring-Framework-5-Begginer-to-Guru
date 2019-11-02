b1fdb6601fe29c33bc5767f1b03b09dbb157eb61
scgr <social.cgr@gmail.com>
2019-11-02 13:25:20 +0100

    Modifications to put in practice the reading of environment variables using spring

--- di-demo-4/src/main/java/guru/springframework/config/PropertyConfig.java
+++ di-demo-4/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package guru.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 
@@ -12,6 +14,9 @@ import guru.springframework.examplebeans.FakeDataSource;
 @PropertySource("classpath:datasource.properties") // Anotacion para indicar cual es el fichero de propiedades del que
 													// va a matchear los valores
 public class PropertyConfig {
+
+	@Autowired
+	Environment environment;
 
 	@Value("${guru.name}") // Anotacion para matchear una propiedad de un fichero con un atributo java
 	String name;
@@ -25,7 +30,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setName(name);
+		fakeDataSource.setName(environment.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 
