491c4f7dfeb4a1c90fe2b19d18718c50a917013b
scgr <social.cgr@gmail.com>
2019-11-04 22:32:22 +0100

    Added PetType service and PetType service map for data in memory

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType entity) {
+		return super.save(entity);
+	}
+
+	@Override
+	public void delete(PetType entity) {
+		super.delete(entity);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
