608a79de33d72f98752d450778ad1340834bd38d
scgr <social.cgr@gmail.com>
2019-11-16 15:18:18 +0100

    Added Ingredient and modified Recipe entities for JPA implementation

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
@@ -1,11 +1,14 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -24,7 +27,10 @@ public class Recipe {
 	private String directions;
 	// TODO To be added
 	// private Difficulty difficulty;
-	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+
 	@Lob
 	private Byte[] image;
 
