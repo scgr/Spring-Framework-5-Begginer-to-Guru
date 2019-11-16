24eaef1647127ac4950ce095f9a254a36865b09a
scgr <social.cgr@gmail.com>
2019-11-16 16:29:28 +0100

    Added Difficulty and modified Recipe entities for JPA implementation

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,8 +27,9 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	// TODO To be added
-	// private Difficulty difficulty;
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
@@ -115,6 +118,22 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
