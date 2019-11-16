0c340465e28ae0a3a277f831c6f9f967e816db12
scgr <social.cgr@gmail.com>
2019-11-16 15:27:39 +0100

    Added UnitOfMeasure and modified Ingredient entities for JPA implementation

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Ingredient.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Ingredient.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -21,8 +22,8 @@ public class Ingredient {
 	@ManyToOne
 	private Recipe recipe;
 
-	// TODO To be added
-	// private UnitOfMeasure uom;
+	@OneToOne
+	private UnitOfMeasure uom;
 
 	public Long getId() {
 		return id;
@@ -54,6 +55,14 @@ public class Ingredient {
 
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
 	}
 
 }
