d332d9be7a2d6e8bd012398304264550ac48efe4
scgr <social.cgr@gmail.com>
2019-11-17 14:53:04 +0100

    Map to JPA implementation the classes Pet and Visit

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -14,17 +18,20 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "name")
 	private String name;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "petType_id")
 	private PetType petType;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Owner owner;
-	
+
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	/**
 	 * @return the name
@@ -80,6 +87,20 @@ public class Pet extends BaseEntity {
 	 */
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	/**
+	 * @return the visits
+	 */
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	/**
+	 * @param visits the visits to set
+	 */
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -2,10 +2,24 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	/**
